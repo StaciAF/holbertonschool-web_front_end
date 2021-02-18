16b64e8232a6851b52ef6f80ee6b9293fab360b0
StaciAF <aaen.it19@gmail.com>
2021-02-17 19:12:52 -0600

    adds function count prime numbers up to 100 with logged perfromance timing

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,20 @@
+let start = performance.now()
+
+function countPrimeNumbers() {
+    var primes = []
+    for (let count = 2; count <= 100; count++) {
+        if (count == 2 | count == 3 | count == 5 | count == 7) {
+            primes.push(count)
+        }
+        else if (count % 2 == 0 | count % 3 == 0 | count % 5 == 0 | count % 7 == 0) {
+            count++;
+        }
+        else {
+            primes.push(count);
+        }
+    }
+    return primes.length
+};
+
+let end = performance.now()
+console.log("Execution time of printing countPrimeNumbers was " + (end - start) + " milliseconds.")
